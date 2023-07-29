c072cdf3f9fee52b45ba56080e05a068c5c8c483
kanghanju <hanju1017@gmail.com>
2023-07-29 23:41:07 +0900

    Dynamic Array- 정수 명령 처리5[2023/07/29]
    ●IntegerCommandProcessing5:정수 명령 처리5

---
+++ CodeTree/src/dataStructure/dynamicArray/IntegerCommandProcessing5.java
@@ -0,0 +1,34 @@
+package dataStructure.dynamicArray;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Iterator;
+
+public class IntegerCommandProcessing5 {
+	static int n;
+
+	public static void main(String[] args) {
+		
+		 ArrayList<Integer> v = new ArrayList<>();
+	        Scanner sc = new Scanner(System.in);
+	        n = sc.nextInt();
+	        
+	        for(int i = 0; i<n ; i++){
+	            String order = sc.next();
+
+	            if(order.equals("push_back")){
+	                int val = sc.nextInt();
+	                v.add(val);
+	            }else if(order.equals("pop_back")){
+	                v.remove(v.size()-1);
+	            }else if(order.equals("size")){
+	                System.out.println(v.size());
+	            }else if(order.equals("get")){
+	                int val = sc.nextInt();
+	                System.out.println(v.get(val-1));
+	            }
+	        }
+	        
+	}
+
+}
